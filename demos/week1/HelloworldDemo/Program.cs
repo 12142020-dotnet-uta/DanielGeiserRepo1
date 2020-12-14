502bf4247848f446f40ada9321be7d9780d77f14
Daniel Geiser <dgmdragon@gmail.com>
2020-12-14 12:15:13 -0800

    Made Helloword project

---
+++ demos/week1/HelloworldDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloworldDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
