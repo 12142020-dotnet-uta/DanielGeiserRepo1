dfadf5b6c1103408f0f40a81c1fc82aa392f9207
Daniel Geiser <dgmdragon@gmail.com>
2020-12-16 08:24:03 -0800

    demo branch made

---
+++ traindemos/demo1/rps_game_no_db/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rps_game_no_db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
